6384892ccd5ae76fade92924881eba3a48e6f093
rikixap <bara.adityaricky@gmail.com>
2020-11-22 11:41:38 +0700

    sdssdsdsdsda

--- src/components/molecules/blogitems/index.js
+++ src/components/molecules/blogitems/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { RegisterBg } from '../../../assets';
+// import { RegisterBg } from '../../../assets';
 import './blogitems.scss';
 import { Button, Gap } from '../../atoms';
 import {useHistory} from 'react-router-dom';
 
-const BlogItem = () => {
+const BlogItem = (props) => {
     const history = useHistory();
+    const {image,title,name,date,body} = props;
     return(
         <div className="blog-item">
-            <img className="image-thumb" src={RegisterBg} alt="post"/>
+            <img className="image-thumb" src={image} alt="post"/>
             <div className="content-detail">
-                <p className="title">Title Blog</p>
-                <p className="author">Author - Date post</p>
-                <p className="body">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <p className="title">{title}</p>
+                <p className="author">{name} - {date}</p>
+                <p className="body">{body}</p>
                 <Gap height={20}/>
                 <Button title="View Detail" onClick={() => history.push('./detail-blog')}/>
             </div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {Button, BlogItem, Gap} from '../../components';
 import "./home.scss";
 import {useHistory} from 'react-router-dom';
+import Axios from 'axios';
 
 const Home = () => {
+    const [dataBlog,setDataBlog] = useState([]);
+    useEffect(() => {
+        Axios.get('http://localhost:4000/v1/blog/posts')
+        .then(result => {
+            console.log('data API',result.data);
+            const responseAPI = result.data;
+            setDataBlog(responseAPI.data)
+        })
+        .catch(err => {
+            console.log('error',err);
+        })
+    },[])
     const history = useHistory();
     return(
         <div className="home-page-wrapper">
@@ -12,10 +25,17 @@ const Home = () => {
             </div>
             <Gap height={20}/>
             <div className="content-wrapper">
-                <BlogItem/>
-                <BlogItem/>
-                <BlogItem/>
-                <BlogItem/>
+                {dataBlog.map(blog => {
+                 return  <BlogItem 
+                 key={blog._id} 
+                 image={`http://localhost:4000/${blog.image}`} 
+                 title={blog.title}
+                 body={blog.body}
+                 name={blog.author.name}
+                 date={blog.createdAt}
+                 /> 
+                 
+                })}
             </div>
             <div className="pagination">
                 <Button title="Previous"/>
